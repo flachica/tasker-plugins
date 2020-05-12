421ea239442040143fbd240c09dd00406119c14e
flachica <flachica@gitlab.freyi.es>
2020-05-12 18:17:29 +0200

    Increase documentantion

--- plugin1/tracktime.py
+++ plugin1/tracktime.py
@@ -14,9 +14,21 @@ class ListBoxRowTodoPlugin1(ListBoxRowTodoSpec):
     @list_box_todospec
     def after_track_time(self, todo, before_started_at, after_started_at, total_time, just_now):
         """
-        todo (todo object. See todotxt.io) Object that hold information
-        started_at (float): Unix time. When 0 the todo has being finalizted
-        total_time: Acumulated time in todo
+        This event is fired after user click on one task or close the app with a task started
+        Take on account that this event is fired per task and one click may fire this event two times:
+        one to finalize previous, other for initialize this
+
+        In the original code only track total time per task. To sum all time inverted in a task
+        you need to know when started it and with the current time you can sum time to total time
+        With this hook you can know when was started and the total time in one step
+
+        Arguments:
+        * todo (todo object. See todotxt.io) Object that hold information
+        * before_started_at: Unix time. Last started time
+        * after_started_at (float): Unix time. If greater than 0 todo has initialized
+                                               else the todo has being finalized
+        * total_time: Acumulated time in todo
+        * just_now: Unix time. Only one time instance accross call's
         """
         print('todo: {}, before_started_at: {}, after_started_at: {}, total_time: {}, just_now: {}'.\
               format(todo.text, before_started_at, after_started_at, total_time, just_now))
